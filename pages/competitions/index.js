530ccc3a8ee1ff39efe7a3655bb0943e85ad47fa
Arsh-Kashyap <arshk102001@gmail.com>
2022-09-26 09:12:30 +0530

    Renamed and improved responsiveness

--- pages/competitions/index.js
+++ pages/competitions/index.js
@@ -1,10 +1,10 @@
-import TechCulturalSelector from '../../components/TechCulturalSelector';
+import TwoHeadingSelector from '../../Components/TwoHeadingSelector';
 
 const competitions = () => {
   return (
-    <TechCulturalSelector
-      technicalImageUrl="https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
-      culturalImageUrl="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
+    <TwoHeadingSelector
+      culturalImageUrl="https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
+      technicalImageUrl="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
       leftRoute="/competitions/technical"
       rightRoute="/competitions/cultural"
     />
