d3209c58e3abe91cdd9538a67f1062cf2aa4c79a
bhavesh <bkpassi2017@gmail.com>
2022-10-29 21:25:22 +0530

    Added Events Listing Page

--- pages/events/megashows/index.js
+++ pages/events/megashows/index.js
@@ -1,8 +1,31 @@
-const technical = () => {
-  return (
-    <div>
-      <h1>Megashows</h1>
-    </div>
-  );
-};
-export default technical;
+import { Grid } from '@mui/material';
+import EventListingCard from '../../../Components/EventListingCard/EventListingCard'
+import styles from '../events.module.css';
+
+const MegaShowEvent = () => {
+
+    return (
+      <div>
+          <h2 className={styles.mainHeading}>MEGA SHOW EVENTS</h2>
+          <Grid
+            container
+            columns={11}
+            rowSpacing={6}
+            columnSpacing={4}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <EventListingCard />
+            <EventListingCard />
+            <EventListingCard />
+            <EventListingCard />
+            <EventListingCard />
+            <EventListingCard />
+            <EventListingCard />
+          </Grid>        
+      </div>
+    );
+}
+
+export default MegaShowEvent;
