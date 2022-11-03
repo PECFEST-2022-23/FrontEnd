5921639ff610c9393f9a7225d335542e0c8651d6
bhavesh <bkpassi2017@gmail.com>
2022-11-03 11:09:54 +0530

    Improved Event Listing UI

--- Components/EventListingCard/EventListingCard.js
+++ Components/EventListingCard/EventListingCard.js
@@ -5,17 +5,38 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import { Button } from '@mui/material';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ShareIcon from '@mui/icons-material/Share';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import borderFrame from "./border-frame-design.png";
 import styles from './EventListingCard.module.css';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 function EventListingCard(props) {
+
+  const [description, setDescription] = useState("A night filled with melodious music, with sensational and A night filled with melodious music, with sensational and A night filled with melodious music, with sensational and ....");
+
+  const processDescriptionMob = (desc) => {
+    let contentLengthMob = 300;
+
+    let trimmedContent = desc.substring(0, contentLengthMob);
+    setDescription(trimmedContent)
+  }
+
+  const processDescriptionPC = (desc) => {
+    let contentLengthPC = 100;
+
+    let trimmedContent = desc.substring(0, contentLengthPC);
+    setDescription(trimmedContent);
+  }
+
+
   const router = useRouter();
   return (
-    <Grid item xs={10} sm={6} md={4} lg={3} xl={3}>
+    <Grid item xs={10} sm={6} md={6} lg={4} xl={4}>
       <div
         onClick={() => router.push('/eventList/' + props.id)}
         className={styles.flipCard}
@@ -23,104 +44,84 @@ function EventListingCard(props) {
         <div className={`${styles.flipCardInner}`}>
           <div className={styles.flipCardFront}>
             <Card className={styles.eventCard}>
-              <div className={styles.borderCardMediaDiv}>
-                <CardMedia
-                  component="img"
-                  height="10%"
-                  image="https://www.kindpng.com/picc/m/93-933681_border-frame-golden-transparent-clip-art-image-gallery.png"
-                  alt="green iguana"
-                  className={styles.borderCardMedia}
-                />
-                <div className={styles.imageCardMediaDiv}>
+            {/* <div className={styles.borderCardMediaDiv}>
+            <div className={styles.imageCardMediaDiv}>
                   <CardMedia
                     component="img"
-                    height="400"
+                    height="250"
                     image="https://image.shutterstock.com/image-vector/urban-techno-music-event-background-600w-47546335.jpg"
                     alt="green iguana"
                     className={styles.imageCardMedia}
                   />
                 </div>
-              </div>
+              </div> */}
+              <div className={styles.borderCardMediaDiv}>
+                <CardMedia
+                >
+                  <Image 
+                    src={require('./border-frame-design.png')}
+                  />
+                </CardMedia>
+                <div className={styles.imageCardMediaDiv}>
+                  <CardMedia
+                    component="img"
+                    height="250"
+                    image="https://image.shutterstock.com/image-vector/urban-techno-music-event-background-600w-47546335.jpg"
+                    alt="green iguana"
+                    className={styles.imageCardMedia}
+                  />
+                </div>
+              </div> 
             </Card>
           </div>
           <div className={styles.flipCardBack}>
             <Card className={styles.eventCard}>
               <div className={styles.borderCardMediaDiv}>
                 <CardMedia
-                  component="img"
-                  height="10%"
-                  image="https://www.kindpng.com/picc/m/93-933681_border-frame-golden-transparent-clip-art-image-gallery.png"
-                  alt="green iguana"
-                  className={styles.borderCardMedia}
-                />
+                >
+                  <Image 
+                    src={require('./border-frame-design.png')}
+                  />
+                </CardMedia> 
                 <div className={styles.imageCardMediaDiv}>
                   <CardContent>
                     <p className={styles.eventElementHeading}>
-                      Musical Night ({props.eventType})
+                      Musical Night 
                     </p>
-
+                    <div style={{textAlign: "center"}}>
+                    <Button variant="contained" size="small">
+                      ({props.eventType})
+                    </Button>
+                    </div>
                     <div className={styles.eventContentDiv}>
                       <p className={styles.eventContent}>
                         A night filled with melodious music, with sensational
-                        and worth listening performances. Dont miss the chance
-                        to wistness it go and get registered. Share this with
-                        your friends to chime them in !
+                        and ....
                       </p>
+                    </div> 
+                    <div className={styles.icons}>
+                      <a href={"https://www.google.com"} target="_blank" rel="noreferrer">
+                        <ExitToAppIcon />
+                      </a>
+                      <Tooltip style={{color: "white"}} title="2nd Nov 22, 5.30 PM">
+                        <CalendarMonthIcon />
+                      </Tooltip>
+                      <Tooltip title="2nd Nov 22, 5.30 PM">
+                        <CalendarMonthIcon />
+                      </Tooltip>
+                      <Tooltip title="College Auditorium  ">
+                        <LocationOnIcon />
+                      </Tooltip> 
+                      <a style={{color: "white"}} href={"https://www.google.com"} target="_blank" rel="noreferrer">
+                        <ShareIcon/>
+                      </a>
+                      <a href={"https://www.google.com"} target="_blank" rel="noreferrer">
+                        <ShareIcon/>
+                      </a>
                     </div>
-                    <Grid container spacing={2}>
-                      <Grid xs={12}>
-                        <IconButton disabled className={styles.iconButton}>
-                          <CalendarMonthIcon />
-                        </IconButton>
-                        <span className={styles.eventTimePlace}>
-                          2nd Nov 22, 5.30 PM
-                        </span>
-                      </Grid>
-                      <Grid xs={12}>
-                        <IconButton disabled className={styles.iconButton}>
-                          <LocationOnIcon />
-                        </IconButton>
-                        <span className={styles.eventTimePlace}>
-                          College Auditorium{' '}
-                        </span>
-                      </Grid>
-                    </Grid>
-                    <Grid
-                      container
-                      columns={10}
-                      columnSpacing={4}
-                      direction="row"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Grid item xs={5} mt={3} mb={3}>
-                        <Button
-                          variant="contained"
-                          className="eventButton"
-                          onClick={() => openInNewTab('https://www.google.com')}
-                          size="large"
-                          color="primary"
-                          startIcon={<ExitToAppIcon />}
-                        >
-                          Register
-                        </Button>
-                      </Grid>
-                      <Grid item xs={5} mt={3} mb={3}>
-                        <Button
-                          variant="contained"
-                          className="eventButton"
-                          onClick={() => openInNewTab('https://www.google.com')}
-                          size="large"
-                          color="primary"
-                          startIcon={<ShareIcon />}
-                        >
-                          Share
-                        </Button>
-                      </Grid>
-                    </Grid>
                   </CardContent>
                 </div>
-              </div>
+              </div> 
             </Card>
           </div>
         </div>
