0f09e4745e7d053f322d7423348ebe96aa5b389c
Puneet Bansal <puneetbansal15801@gmail.com>
2022-11-15 11:32:08 +0530

    Revert "fixed ui of specific event page"
    This reverts commit 717568b821cd69808ec282fb2850fb3c3b24b7bf.

--- Components/Loader/index.js
+++ Components/Loader/index.js
@@ -3,14 +3,10 @@ import styles from './Loader.module.css';
 import Head from 'next/head';
 
 const Loader = () => {
-  return (
-    <div className={styles.loader_container}>
-      <Head>
-        <title>Loading...</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-    </div>
-  );
+  return <div className={styles.loader_container}><Head>
+  <title>Loading...</title>
+  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+</Head></div>;
 };
 
 export default Loader;
